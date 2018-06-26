16f11e556317685175d5c31664adfbb04436a8ef
SudoRB <40261119+SudoRB@users.noreply.github.com>
2018-06-26 16:03:25 +0200

    Added Portal

---
+++ Assets/PortalScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalScript : MonoBehaviour {
+
+    public Camera camera =FindObjectOfType<Camera>();
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GetComponent<Transform>().LookAt(camera.GetComponent<Transform>());
+	}
+}
